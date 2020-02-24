77fef03ac77b611d221803d8672cbf686762aeea
kpensec <kevin.pensec@gmail.com>
2020-02-25 00:24:50 +0100

    completed? (missing testing cases)

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from __future__ import printfunction
+from __future__ import print_function
 
 """
   deflating key from yaml file with '.' in keys
@@ -9,20 +9,45 @@ from __future__ import printfunction
 def usage():
     print(f"{__file__} <yaml file>")
 
-def deflate(data : Union[dict, list]:
-    result = data
+def _deflate_dict(data: dict):
+    result = {}
+    for k,v in data.items():
+        splited_key = k.split('.')
+        if len(splited_key) == 1:
+            result[k] = deflate(v)
+        else:
+            result[splited_key[0]] = result.get(splited_key[0], {})
+            subresult = result[splited_key[0]]
+            # fetch subkey and alloca missing ones
+            for subk in splited_key[1:-1]:
+                subresult[subk] = subresult.get(subk, {})
+                subresult = subresult[subk]
+            subresult[splited_key[-1]] = deflate(v)
+
     return result
 
+
+def deflate(data):
+    return {
+        dict: _deflate_dict,
+        list: lambda x: list(map(deflate,x))
+    }.get(type(data), lambda x:x)(data)
+
 def main(argv):
-    import yaml
+    data = {
+        "a.b": [{"d.e": 2}, {"d.f": 4}],
+        "a.g": "testing",
+        "a.h.f.a.b": 42,
+        "a.h.f.b.c": 42,
+        "a.h.g": 42,
+        "b": 14,
+        "c": "testing string",
+        "g": {"a.f.g": 42},
+        "g": {"a":{"f":{"z":5}}},
+        "g": {"borzaz": 55},
+    }
 
-    if len(argv) != 2:
-        usage()
-    filename = argv[1]
-    
-    data = yaml.load(open(filename))
-
-    deflate_key(data)
+    return deflate(data)
         
 
 if __name__ == "__main__":
